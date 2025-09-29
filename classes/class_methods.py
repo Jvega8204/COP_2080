1b43bece26563256cba7d5ba4dafb8f16e865c61
Jvega8204 <jvega8204@floridapoly.edu>
2025-09-29 17:40:06 +0000

    Class methods

---
+++ classes/class_methods.py
@@ -0,0 +1,27 @@
+class book:
+    total_books = 0 # total number of books created
+
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+        Book.total_books += 1
+
+    
+    # I want to create a method which will work with total_books
+    #class method
+    @classmethod
+    def reset_total_books(cls):
+        """Reset the total book count to zero"""
+        cls.total_books = 0
+
+    @classmethod
+    def from_dict(cls, data: dict):
+        return cls(data["title"], data["author"], data["year"])
+    
+book1 = Book("Python 101", "J.Doe", 2023)
+data = {"title": "Learning Python", "author": "John smith", "year": 2020}
+book2 = Book.from_dict(data)
+
+print (book1)
+print (book2)
