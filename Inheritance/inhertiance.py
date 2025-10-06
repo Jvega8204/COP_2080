3a00e6253942d5491400e36c3d63b7fc1ec08783
Jvega8204 <jvega8204@floridapoly.edu>
2025-10-06 17:19:33 +0000

    Inheritance

---
+++ Inheritance/inhertiance.py
@@ -0,0 +1,15 @@
+class student: 
+    def __init__(self, name, student_id, major):
+        self.name = name 
+        self.student_id = student_id
+        self.major = major
+    def __str__(self):
+        return f"Name: {self.name}, ID: {self.student_id}, Major: {self.major}"
+class UndegraduateStudent:
+    def __init__(self, name, student_id, major):
+        self.name = name 
+        self.student_id = student_id
+        self.major = major
+if __name__ == "__main__":
+    student = Student("Jack", "U1010", "Computer Science")
+    print(student)
