04747cf2972b5b287a154443a46db8d488670c64
Jvega8204 <jvega8204@floridapoly.edu>
2025-11-05 18:53:14 +0000

    gui

--- gui_layout/gui_layout.py
+++ gui_layout/gui_layout.py
@@ -37,7 +37,7 @@ with ui.row():
         with ui.card().classes("h-65 w-full"):
             input_field = ui.input("Enter your name")
             ui.button("Greet me!", color = "green", on_click = greet)
-
+    with ui.column().classes("flex-1"):
         with ui.card().classes("h-65 w-full"): 
             count_label = ui.label("Count: 0 ").classes("text-lg")
             with ui.row():
@@ -46,6 +46,15 @@ with ui.row():
             label_slider = ui.label("step: 5").classes("text-lg")
             slider = ui.slider(min=1, max = 10, value = 5)
             slider.on("update:model-value", lambda: label_slider.set_text(f"Step: {slider.value}"))
+
+    with ui.row():
+        n1 = ui.number("Number 1",value = 0).classes("w-24")
+        ui.label("+").classes("text-lg")
+        n2 = ui.number("Number 2",value = 0).classes("w-24")
+        ui.label("=").classes("text-lg")
+        ui.label(f"{n1}+{n2}")
+
+
 
 ui.switch ("Darkmode", 
         on_change=lambda e: ui.dark_mode().enable() if e.value else ui.dark_mode().disable())
